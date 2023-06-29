a5ad251db8ad6cf8ec1d58012221e5f1b9124b04
Alex Robbins <alexanderkrobbins@gmail.com>
2023-06-29 13:21:34 -0600

    Finished vulkan pt. 1

--- src/vk_engine.h
+++ src/vk_engine.h
@@ -45,10 +45,31 @@ public:
 	//array of image-views from the swapchain
 	std::vector<VkImageView> _swapchainImageViews;
 
+	VkQueue _graphicsQueue; // queue we will submit to 
+	uint32_t _graphicsQueueFamily; // family of that queue
+
+	VkCommandPool _commandPool; // the command pool for our commands
+	VkCommandBuffer _mainCommandBuffer; // the buffer we will record to
+
+	VkRenderPass _renderPass;
+
+	std::vector<VkFramebuffer> _framebuffers;
+
+	VkSemaphore _presentSemaphore, _renderSemaphore;
+	VkFence _renderFence;
+
 private:
 
 	void init_vulkan();
 
 	void init_swapchain();
+
+	void init_commands();
+
+	void init_default_renderpass();
+
+	void init_framebuffers();
+
+	void init_sync_structures();
 
 };
--- src/vk_initializers.cpp
+++ src/vk_initializers.cpp
@@ -1,1 +1,23 @@
 ﻿#include <vk_initializers.h>
+
+
+VkCommandPoolCreateInfo vkinit::command_pool_create_info(uint32_t queueFamilyIndex, VkCommandPoolCreateFlags flags /*=0*/) {
+	VkCommandPoolCreateInfo info = {}; // initializes entire struct to zero
+	info.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+	info.pNext = nullptr;
+
+	info.queueFamilyIndex = queueFamilyIndex;
+	info.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
+	return info;
+}
+
+VkCommandBufferAllocateInfo vkinit::command_buffer_allocate_info(VkCommandPool pool, uint32_t count /*= 1*/, VkCommandBufferLevel level /*= VK_COMMAND_BUFFER_LEVEL_PRIMARY*/) {
+	VkCommandBufferAllocateInfo info = {};
+	info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+	info.pNext = nullptr;
+
+	info.commandPool = pool;
+	info.commandBufferCount = 1;
+	info.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+	return info;
+}
