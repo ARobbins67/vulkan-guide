2e872a9b59b75abe41f382c25b1b0d7a7a4cfcc1
Alex Robbins <47987771+ARobbins67@users.noreply.github.com>
2023-07-02 14:09:00 -0600

    Some progress on triangle rendering

--- src/vk_engine.h
+++ src/vk_engine.h
@@ -3,18 +3,42 @@
 
 #pragma once
 
-#include <vector>
 #include <vk_types.h>
+#include <vector>
 
 class VulkanEngine {
 public:
 
 	bool _isInitialized{ false };
-	int _frameNumber {0};
+	int _frameNumber{ 0 };
 
 	VkExtent2D _windowExtent{ 1700 , 900 };
 
 	struct SDL_Window* _window{ nullptr };
+
+	VkInstance _instance;
+	VkDebugUtilsMessengerEXT _debug_messenger;
+	VkPhysicalDevice _chosenGPU;
+	VkDevice _device;
+
+	VkSemaphore _presentSemaphore, _renderSemaphore;
+	VkFence _renderFence;
+
+	VkQueue _graphicsQueue;
+	uint32_t _graphicsQueueFamily;
+
+	VkCommandPool _commandPool;
+	VkCommandBuffer _mainCommandBuffer;
+
+	VkRenderPass _renderPass;
+
+	VkSurfaceKHR _surface;
+	VkSwapchainKHR _swapchain;
+	VkFormat _swachainImageFormat;
+
+	std::vector<VkFramebuffer> _framebuffers;
+	std::vector<VkImage> _swapchainImages;
+	std::vector<VkImageView> _swapchainImageViews;
 
 	//initializes everything in the engine
 	void init();
@@ -28,52 +52,36 @@ public:
 	//run main loop
 	void run();
 
-	VkInstance _instance; // Vulkan library handle
-	VkDebugUtilsMessengerEXT _debug_messenger; // Vulkan debug output handle
-	VkPhysicalDevice _chosenGPU; // GPU chosen as default device
-	VkDevice _device; // Vulkan device for commands
-	VkSurfaceKHR _surface; // Vulkan window surface
-
-	VkSwapchainKHR _swapchain;
-
-	// image format expected by the windowing system
-	VkFormat _swapchainImageFormat;
-
-	// array of images from the swapchain
-	std::vector<VkImage> _swapchainImages;
-
-	//array of image-views from the swapchain
-	std::vector<VkImageView> _swapchainImageViews;
-
-	VkQueue _graphicsQueue; // queue we will submit to 
-	uint32_t _graphicsQueueFamily; // family of that queue
-
-	VkCommandPool _commandPool; // the command pool for our commands
-	VkCommandBuffer _mainCommandBuffer; // the buffer we will record to
-
-	VkRenderPass _renderPass;
-
-	std::vector<VkFramebuffer> _framebuffers;
-
-	VkSemaphore _presentSemaphore, _renderSemaphore;
-	VkFence _renderFence;
-
 private:
 
 	void init_vulkan();
 
 	void init_swapchain();
 
-	void init_commands();
-
 	void init_default_renderpass();
 
 	void init_framebuffers();
 
+	void init_commands();
+
 	void init_sync_structures();
 
-	// loads a shader module from a spir-v file. returns false if it errors
-	bool load_shader_module(const char* filePaht, VkShaderModule* outShaderModule);
-
 	void init_pipelines();
+
+	bool load_shader_module(const char* filePath, VkShaderModule* outShaderModule);
+};
+
+class PipelineBuilder {
+public:
+	std::vector<VkPipelineShaderStageCreateInfo> _shaderStages;
+	VkPipelineVertexInputStateCreateInfo _vertexInputInfo;
+	VkPipelineInputAssemblyStateCreateInfo _inputAssembly;
+	VkViewport _viewport;
+	VkRect2D _scissor;
+	VkPipelineRasterizationStateCreateInfo _rasterizer;
+	VkPipelineColorBlendAttachmentState _colorBlendAttachment;
+	VkPipelineMultisampleStateCreateInfo _multisampling;
+	VkPipelineLayout _pipelineLayout;
+
+	VkPipeline build_pipeline(VkDevice device, VkRenderPass pass);
 };
